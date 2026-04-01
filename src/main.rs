3982614916ed9d54c65c57ae507593fbbd2b2129
Chris Brenton <hiring@rubyridge.org>
2026-04-01 12:41:57 -0400

    Add glium and draw a blank blue screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use glium::Surface;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = glium::winit::event_loop::EventLoopBuilder::new()
+        .build()
+        .expect("event loop building");
+    let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
+
+    let mut frame = display.draw();
+    frame.clear_color(0.0, 0.0, 1.0, 1.0);
+    frame.finish().unwrap();
+
+    let _ = event_loop.run(move |event, window_target| {
+        match event {
+            glium::winit::event::Event::WindowEvent { event, .. } => match event {
+                glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
+                _ => (),
+            },
+            _ => (),
+        };
+    });
 }
