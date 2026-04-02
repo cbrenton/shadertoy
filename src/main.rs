4fe8fa4038101febf7d4ea0214125b19bf9cbc91
Chris Brenton <hiring@rubyridge.org>
2026-04-02 13:37:45 -0400

    Move more stuff into render module, and draw each program each frame

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-use glium::Surface;
-
 mod render;
-
-#[derive(Copy, Clone)]
-struct Vertex {
-    position: [f32; 2],
-}
-implement_vertex!(Vertex, position);
 
 #[macro_use]
 extern crate glium;
@@ -16,41 +8,21 @@ fn main() {
         .expect("event loop building");
     let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
 
-    let mut shader_mgr = render::ShaderManager::new(display, "./resources/shaders/");
-    shader_mgr.load_shader("hello_vert", "hello_frag");
-
-    let positions = [[-0.5, -0.5], [0.0, 0.5], [0.5, -0.25]];
-    let mut tris = vec![];
-    for position in positions {
-        tris.push(Vertex { position });
-    }
-
-    // upload vertices to vertex buffer
-    let vertex_buffer = glium::VertexBuffer::new(&shader_mgr.display, &tris).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
-    let mut target = shader_mgr.target();
-    target.clear_color(0.0, 0.0, 1.0, 1.0);
-
-    let program = &shader_mgr.programs[0];
-    target
-        .draw(
-            &vertex_buffer,
-            indices,
-            program,
-            &glium::uniforms::EmptyUniforms,
-            &Default::default(),
-        )
-        .unwrap();
-    target.finish().unwrap();
+    let mut render_mgr = render::RenderManager::new(display, "./resources/shaders/");
+    render_mgr.load_shader("helloworld", "hello_vert", "hello_frag");
 
     let _ = event_loop.run(move |event, window_target| {
         match event {
             glium::winit::event::Event::WindowEvent { event, .. } => match event {
                 glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
+                glium::winit::event::WindowEvent::Resized(new_size) => {
+                    render_mgr.resize(new_size);
+                }
                 _ => (),
             },
-            _ => (),
+            _ => {
+                render_mgr.draw_frame();
+            }
         };
     });
 }
--- src/render.rs
+++ src/render.rs
@@ -1,23 +1,31 @@
+use glium::Surface;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
 extern crate glium;
 
-pub struct ShaderManager {
+#[derive(Copy, Clone)]
+pub struct Vertex {
+    pub position: [f32; 2],
+}
+implement_vertex!(Vertex, position);
+
+pub struct RenderManager {
     pub display: glium::Display<glium::glutin::surface::WindowSurface>,
-    pub programs: Vec<glium::Program>,
+    pub programs: HashMap<String, ShaderProgram>,
     pub shader_path: String,
 }
 
-impl ShaderManager {
+impl RenderManager {
     pub fn new(
         display: glium::Display<glium::glutin::surface::WindowSurface>,
         shader_path: &str,
     ) -> Self {
         Self {
             display,
-            programs: Default::default(),
+            programs: HashMap::new(),
             // TODO: make sure there's a trailing slash
             shader_path: shader_path.to_string(),
         }
@@ -28,12 +36,23 @@ impl ShaderManager {
     }
 
     // TODO: default to hello world
-    pub fn load_shader(&mut self, vert_name: &str, frag_name: &str) {
+    pub fn load_shader(&mut self, shader_name: &str, vert_name: &str, frag_name: &str) {
         let vert_src = self.read_shader_file(&format!("{}/{}.glsl", self.shader_path, vert_name));
         let frag_src = self.read_shader_file(&format!("{}/{}.glsl", self.shader_path, frag_name));
 
+        let positions = [[-0.5, -0.5], [0.0, 0.5], [0.5, -0.25]];
+        let mut tris = vec![];
+        for position in positions {
+            tris.push(Vertex { position });
+        }
+        let vertex_buffer = glium::VertexBuffer::new(&self.display, &tris).unwrap();
+
         let program = self.create_program(&vert_src, &frag_src);
-        self.programs.push(program);
+
+        let shader_program = ShaderProgram::new(program, vertex_buffer);
+
+        self.programs
+            .insert(shader_name.to_string(), shader_program);
     }
 
     fn create_program(&mut self, vert_src: &str, frag_src: &str) -> glium::Program {
@@ -56,6 +75,43 @@ impl ShaderManager {
         match file.read_to_string(&mut src) {
             Err(why) => panic!("couldn't read {}: {}", path_display, why),
             Ok(_) => src,
+        }
+    }
+
+    pub fn draw_frame(&mut self) {
+        let mut target = self.target();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        for (name, program) in &self.programs {
+            println!("drawing {name}");
+            target
+                .draw(
+                    &program.vertex_buffer,
+                    glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
+                    &program.program,
+                    &glium::uniforms::EmptyUniforms,
+                    &Default::default(),
+                )
+                .unwrap();
+        }
+        target.finish().unwrap();
+    }
+
+    pub fn resize(&mut self, new_size: glium::winit::dpi::PhysicalSize<u32>) {
+        self.display.resize(new_size.into())
+    }
+}
+
+pub struct ShaderProgram {
+    pub vertex_buffer: glium::VertexBuffer<Vertex>,
+    pub program: glium::Program,
+}
+
+impl ShaderProgram {
+    pub fn new(program: glium::Program, vertex_buffer: glium::VertexBuffer<Vertex>) -> Self {
+        Self {
+            program,
+            vertex_buffer,
         }
     }
 }
